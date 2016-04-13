1f644ab055a20f636acd2e436b4c2b6eefc809fd
steveshortt <steve.shortt@gmail.com>
2016-04-12 20:39:38 -0500

    working on prototype workflow object model

---
+++ Synapse.Core/Plan.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Synapse.Core
+{
+	public class Plan
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		//public Design Process { get; set; }
+		public List<ActionItem> Actions { get; set; }
+		public bool IsActive { get; set; }
+	}
+
+	//public class Design
+	//{
+	//	public List<ActionItem> Actions { get; set; }
+	//}
+
+	public class ActionItem
+	{
+		public string Name { get; set; }
+		public string GroupKey { get; set; }
+		public string ResultCase { get; set; }
+		public Handler Handler { get; set; }
+		public Parameters Parameters { get; set; }
+		public List<ActionItem> Actions { get; set; }
+
+		public static ActionItem Create(string name = "xxx")
+		{
+			return new ActionItem()
+			{
+				Name = name,
+				GroupKey = "meow",
+				Handler = new Handler() { Type = "yyy", ConfigKey = "zzz" },
+				Parameters = new Parameters() { Values = "foo" },
+				Actions = new List<ActionItem>()
+			};
+		}
+	}
+
+	public class Parameters
+	{
+		public string Uri { get; set; }
+		public object Values { get; set; }
+	}
+
+	public class Handler
+	{
+		public string Type { get; set; }
+		public string ConfigKey { get; set; }
+		public string ConfigValues { get; set; }
+	}
+
+	public enum ActionType
+	{
+		Action,
+		Group
+	}
+}
---
+++ Synapse.Tester/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Synapse.Core;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+
+namespace Synapse.Tester
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if( args.Length > 0 )
+			{
+				using( StreamReader sr = new StreamReader( @"yaml.txt" ) )
+				{
+					Deserializer deserializer = new Deserializer( ignoreUnmatched: false );
+					Plan plan = deserializer.Deserialize<Plan>( sr );
+				}
+			}
+
+			ActionItem ac2 = ActionItem.Create( "ac2" );
+			ActionItem ac1 = ActionItem.Create( "ac1" );
+			ActionItem ac0 = ActionItem.Create( "ac0" );
+			ac0.Actions = new List<ActionItem>();
+			ac0.Actions.Add( ac1 );
+			List<ActionItem> actions = new List<ActionItem>();
+			actions.Add( ac0 );
+			actions.Add( ac2 );
+
+			//Design pr = new Design()
+			//{
+			//	Actions = actions
+			//};
+
+			Plan p = new Plan()
+			{
+				Name = "plan0",
+				//Process = pr,
+				Actions = actions,
+				Description = "planDesc",
+				IsActive = true
+			};
+
+			using( StreamWriter file = new StreamWriter( @"yaml.txt" ) )
+			{
+				Serializer serializer = new Serializer();
+				serializer.Serialize( file, p );
+			}
+			Console.Read();
+		}
+	}
+}
