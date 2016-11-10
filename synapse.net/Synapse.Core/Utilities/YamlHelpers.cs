44691b2af0df34154dd92e1e1ea2037f29ee590f
steveshortt <steve.shortt@gmail.com>
2016-11-10 13:33:25 -0600

    update to YamlHelpers

--- synapse.net/Synapse.Core/Utilities/YamlHelpers.cs
+++ synapse.net/Synapse.Core/Utilities/YamlHelpers.cs
@@ -1,21 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using YamlDotNet.Serialization;
 
 namespace Synapse.Core.Utilities
 {
     public class YamlHelpers
     {
-        public static T Deserialize<T>(TextReader reader)
+        public static T Deserialize<T>(TextReader reader, bool ignoreUnmatchedProperties = true)
         {
             DeserializerBuilder builder = new DeserializerBuilder();
-            builder.IgnoreUnmatchedProperties();
+            if( ignoreUnmatchedProperties )
+                builder.IgnoreUnmatchedProperties();
             Deserializer deserializer = builder.Build();
             return deserializer.Deserialize<T>( reader );
+        }
+
+        public void Serialize(TextWriter tw, object data)
+        {
+            Serializer serializer = new Serializer();
+            serializer.Serialize( tw, data );
+        }
+
+        public string Serialize(object data)
+        {
+            string result = null;
+            using( StringWriter writer = new StringWriter() )
+            {
+                Serializer serializer = new Serializer();
+                serializer.Serialize( writer, data );
+                result = writer.ToString();
+            }
+            return result;
         }
     }
 }
