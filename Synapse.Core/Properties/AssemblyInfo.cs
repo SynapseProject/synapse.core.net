075469a0dd480b87902eaa3f0aaac382fcdb61b6
steveshortt <steve.shortt@gmail.com>
2017-01-01 13:27:34 -0600

    Switching to signed versions of libs, signing Synapse.

--- Synapse.Core/Properties/AssemblyInfo.cs
+++ Synapse.Core/Properties/AssemblyInfo.cs
@@ -1,18 +1,22 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "Synapse.Core" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "Core classes, workflow engine." )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "Synapse.Core" )]
-[assembly: AssemblyCopyright( "Copyright ©  Steve Shortt, 2016" )]
+[assembly: AssemblyCopyright( "Copyright ©  Steve Shortt, 2016 - 2017" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
+
+[assembly: AllowPartiallyTrustedCallers( PartialTrustVisibilityLevel = PartialTrustVisibilityLevel.VisibleToAllHosts )]
+[assembly: AssemblyKeyFile( "Synapse.Core.snk" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +36,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "0.0.0.3" )]
+[assembly: AssemblyFileVersion( "0.0.0.3" )]
