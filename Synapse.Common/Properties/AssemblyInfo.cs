5f1d14d4e60dc9a009fc6ffb75a22508c41375a2
steveshortt <steve.shortt@gmail.com>
2017-01-09 09:24:49 -0600

    version rev on Common, Wpf

--- Synapse.Common/Properties/AssemblyInfo.cs
+++ Synapse.Common/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "Synapse.Service.Common" )]
+[assembly: AssemblyTitle( "Synapse.Common" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "Synapse.Service.Common" )]
-[assembly: AssemblyCopyright( "Copyright ©  Steve Shortt, 2016" )]
+[assembly: AssemblyProduct( "Synapse.Common" )]
+[assembly: AssemblyCopyright( "Copyright ©  Steve Shortt, 2016 - 2017" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "0.0.0.5" )]
+[assembly: AssemblyFileVersion( "0.0.0.5" )]
