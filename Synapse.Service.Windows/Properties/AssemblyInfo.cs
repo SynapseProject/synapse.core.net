3f5b3a05607bab555faf848545f2ccde6db39516
steveshortt <steve.shortt@gmail.com>
2017-01-09 09:22:26 -0600

    version rev on service components

--- Synapse.Service.Windows/Properties/AssemblyInfo.cs
+++ Synapse.Service.Windows/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "Synapse.Service.Windows" )]
-[assembly: AssemblyCopyright( "Copyright ©  Steve Shortt, 2016" )]
+[assembly: AssemblyCopyright( "Copyright ©  Steve Shortt, 2016 - 2017" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "0.0.0.5" )]
+[assembly: AssemblyFileVersion( "0.0.0.5" )]
