8b64315bb0c861b3d1c01a6b2627732e1361764f
steveshortt <steve.shortt@gmail.com>
2017-01-25 18:52:04 -0600

    Common: added regular exceptions to UnwindException

--- Synapse.Common/WebClient/Utilities.cs
+++ Synapse.Common/WebClient/Utilities.cs
@@ -8,32 +8,59 @@ namespace Synapse.Common.WebApi
     {
         public static string UnwindException(Exception ex)
         {
+            return UnwindException( null, ex );
+        }
+        public static string UnwindException(string context, Exception ex, bool asSingleLine = false)
+        {
+            string lineEnd = asSingleLine ? "|" : @"\r\n";
+
             StringBuilder msg = new StringBuilder();
-            msg.AppendLine( ex.Message );
-            if( ex.InnerException != null && ex.InnerException is AggregateException )
+            if( !string.IsNullOrWhiteSpace( context ) )
+                msg.Append( $"An error occurred in: {context}{lineEnd}" );
+
+            msg.Append( $"{ex.Message}{lineEnd}" );
+
+            if( ex.InnerException != null )
             {
-                AggregateException ae = ex.InnerException as AggregateException;
-                foreach( WebApiClientException wcx in ae.InnerExceptions )
+                if( ex.InnerException is AggregateException )
+                {
+                    AggregateException ae = ex.InnerException as AggregateException;
+                    foreach( WebApiClientException wcx in ae.InnerExceptions )
+                    {
+                        Stack<Exception> exceptions = new Stack<Exception>();
+                        exceptions.Push( wcx );
+
+                        while( exceptions.Count > 0 )
+                        {
+                            Exception e = exceptions.Pop();
+
+                            if( e.InnerException != null )
+                                exceptions.Push( e.InnerException );
+
+                            msg.Append( $"{e.Message}{lineEnd}" );
+
+                            if( e is WebApiClientException && ((WebApiClientException)e).Details != null )
+                                msg.Append( $"{((WebApiClientException)e).Details.Message}{lineEnd}" );
+                        }
+                    }
+                }
+                else
                 {
                     Stack<Exception> exceptions = new Stack<Exception>();
-                    exceptions.Push( wcx );
+                    exceptions.Push( ex.InnerException );
 
                     while( exceptions.Count > 0 )
                     {
                         Exception e = exceptions.Pop();
+                        msg.Append( $"{e.Message}{lineEnd}" );
 
                         if( e.InnerException != null )
                             exceptions.Push( e.InnerException );
-
-                        msg.AppendLine( e.Message );
-
-                        if( e is WebApiClientException && ((WebApiClientException)e).Details != null )
-                            msg.AppendLine( ((WebApiClientException)e).Details.Message );
                     }
                 }
             }
 
-            return msg.ToString();
+            return asSingleLine ? msg.ToString().TrimEnd( '|' ) : msg.ToString();
         }
     }
 }
