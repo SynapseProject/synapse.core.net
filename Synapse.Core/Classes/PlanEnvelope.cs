d2d7b25d7b2b1843f2909cf1a6f0a6a881c4d9f9
steveshortt <steve.shortt@gmail.com>
2017-03-15 23:32:16 -0500

    StartPlanEnvelope - code cleanup

--- Synapse.Core/Classes/PlanEnvelope.cs
+++ Synapse.Core/Classes/PlanEnvelope.cs
@@ -8,23 +8,7 @@ namespace Synapse.Core
     public class StartPlanEnvelope
     {
         public Plan Plan { get; set; }
-        //public List<KeyValueItem> DynamicParameters { get; set; } = new List<KeyValueItem>();
         public Dictionary<string, string> DynamicParameters { get; set; }
-
-        //public void SetDynamicParameters(Dictionary<string, string> dynamicParameters)
-        //{
-        //    foreach( KeyValuePair<string, string> kvp in dynamicParameters )
-        //        DynamicParameters.Add( new KeyValueItem( kvp ) );
-        //}
-        //public Dictionary<string, string> GetDynamicParameters()
-        //{
-        //    Dictionary<string, string> dynamicParameters = new Dictionary<string, string>();
-        //    foreach( KeyValueItem kvi in DynamicParameters )
-        //        dynamicParameters.Add( kvi.Key, kvi.Value );
-
-        //    return dynamicParameters;
-        //}
-
 
         public string ToYaml(bool encode = false)
         {
@@ -47,17 +31,4 @@ namespace Synapse.Core
             return YamlHelpers.Deserialize<StartPlanEnvelope>( yaml );
         }
     }
-
-    //public class KeyValueItem
-    //{
-    //    public KeyValueItem() { }
-    //    public KeyValueItem(KeyValuePair<string, string> kvp)
-    //    {
-    //        Key = kvp.Key;
-    //        Value = kvp.Value;
-    //    }
-
-    //    public string Key { get; set; }
-    //    public string Value { get; set; }
-    //}
 }
