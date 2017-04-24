52bc5689276ae36f8a10d67f454fc62f4eefa291
steveshortt <steve.shortt@gmail.com>
2017-04-24 01:18:40 -0500

    added PlanElementParms

---
+++ Synapse.Core/Classes/PlanElementParms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Synapse.Core.Utilities;
+
+namespace Synapse.Core
+{
+    public class PlanElementParms
+    {
+        public SerializationType Type { get; set; }
+        public List<string> ElementPaths { get; set; }
+    }
+}
