ad096388d6759c05358f686b9c2689f5692b5e11
steveshortt <steve.shortt@gmail.com>
2017-05-08 19:38:44 -0500

    Add support for Find/Replace in DynamicValue (#58)

--- Synapse.Core/Classes/DynamicValue.cs
+++ Synapse.Core/Classes/DynamicValue.cs
@@ -7,11 +7,13 @@ namespace Synapse.Core
     {
         public string Name { get; set; }
         public string Path { get; set; }
+        public string Replace { get; set; }
+        public string Encode { get; set; }
         public List<Option> Options { get; set; }
 
         public override string ToString()
         {
-            return $"[{Name}]::[{Path}]";
+            return $"[{Name}]::[{Path}]::[{Replace}]::[{Encode}]";
         }
     }
     public class Option
