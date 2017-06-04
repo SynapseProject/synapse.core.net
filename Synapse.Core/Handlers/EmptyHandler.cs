5ebfd7387edbd1553a149e743bbc0643a48a9bf9
steveshortt <steve.shortt@gmail.com>
2017-06-04 12:06:47 -0500

    Added 'Sleep' to EmptyHandler

--- Synapse.Core/Handlers/EmptyHandler.cs
+++ Synapse.Core/Handlers/EmptyHandler.cs
@@ -5,10 +5,16 @@ using Synapse.Core;
 public class EmptyHandler : HandlerRuntimeBase
 {
     public override object GetConfigInstance() { return null; }
-    public override object GetParametersInstance() { return new EmptyHandlerParameters() { ReturnStatus = StatusType.Success, ExitData = "Sample Value" }; }
+    public override object GetParametersInstance() { return new EmptyHandlerParameters() { ReturnStatus = StatusType.Success, SleepMilliseconds = 1000, ExitData = "Sample Value" }; }
     override public ExecuteResult Execute(HandlerStartInfo startInfo)
     {
         EmptyHandlerParameters parms = DeserializeOrNew<EmptyHandlerParameters>( startInfo.Parameters );
+
+        if( parms.SleepMilliseconds > 0 )
+        {
+            OnProgress( "Execute", $"Sleeping for {parms.SleepMilliseconds}", StatusType.Waiting, startInfo.InstanceId, 1 );
+            System.Threading.Thread.Sleep( parms.SleepMilliseconds );
+        }
 
         OnProgress( "Execute", parms.ReturnStatus.ToString(), parms.ReturnStatus, startInfo.InstanceId, Int32.MaxValue );
 
@@ -24,6 +30,7 @@ public class EmptyHandlerParameters
         ExitData = "EmptyHandler ExitData default value.";
     }
 
+    public int SleepMilliseconds { get; set; }
     public StatusType ReturnStatus { get; set; }
     public string ExitData { get; set; }
 }
