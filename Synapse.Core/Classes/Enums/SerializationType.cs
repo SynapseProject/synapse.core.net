4bebd1243011a6bd1ded7a5e02c3528cbaed80ee
steveshortt <steve.shortt@gmail.com>
2017-06-15 17:02:14 -0500

    added SerializationType.Html (#62)

--- Synapse.Core/Classes/Enums/SerializationType.cs
+++ Synapse.Core/Classes/Enums/SerializationType.cs
@@ -7,6 +7,28 @@ namespace Synapse.Core
         Yaml,
         Xml,
 		Json,
+        Html,
 		Unspecified
 	}
+
+    public struct SerializationContentType
+    {
+        public const string Yaml = "application/yaml";
+        public const string Xml = "application/xml";
+        public const string Json = "application/json";
+        public const string Html = "text/html";
+        public const string Unspecified = "text/plain";
+
+        public static string GetContentType(SerializationType serializationType)
+        {
+            switch(serializationType)
+            {
+                case SerializationType.Xml: { return Xml; }
+                case SerializationType.Json: { return Json; }
+                case SerializationType.Html: { return Html; }
+                case SerializationType.Unspecified: { return Unspecified; }
+                default: { return Yaml; }
+            }
+        }
+    }
 }
