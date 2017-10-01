d25f7faa60cba84692f7f2652b3cc364e7d1c7f3
LAPTOP-TK2D9TB6\Steve <steve.shortt@gmail.com>
2017-10-01 09:06:28 -0500

    adjusted how AboutData is defined

---
+++ Synapse.Common/Classes/FileEnumerator/VersionData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Synapse.Common.Utilities
+{
+    public class VersionData
+    {
+        public string FileVersion { get; set; }
+        public string FileDescription { get; set; }
+        public string ProductName { get; set; }
+        public string ProductVersion { get; set; }
+
+        public override string ToString()
+        {
+            return $"\"{FileVersion}\",\"{FileDescription}\",\"{ProductName}\",\"{ProductVersion}\""; ;
+        }
+    }
+}
