2dbc22389f97c31ee45fa0e62b945aa02e074143
LAPTOP-TK2D9TB6\Steve <steve.shortt@gmail.com>
2017-10-01 10:01:52 -0500

    renamed AboutBase to AboutData and removed abstract

---
+++ Synapse.Common/Classes/FileEnumerator/AboutData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Synapse.Common.Utilities
+{
+    public class AboutData
+    {
+        public object Config { get; set; }
+
+        public List<FileData> Files { get; set; } = null;
+        public string FilesCsv { get; set; } = null;
+
+        public virtual void GetFiles(bool asCsv = false)
+        {
+            string currentPath = Path.GetDirectoryName( typeof( AboutData ).Assembly.Location );
+            if( asCsv )
+                FilesCsv = FileEnumerator.EnumerateFilesToCsv( currentPath );
+            else
+                Files = FileEnumerator.EnumerateFiles( currentPath );
+        }
+    }
+}
