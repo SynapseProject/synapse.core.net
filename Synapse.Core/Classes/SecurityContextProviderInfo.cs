ac27644dc43213f8a79d7e4041f088639bb9e434
Steve <steve.shortt@gmail.com>
2018-11-30 06:53:39 -0600

    code cleanup

---
+++ Synapse.Core/Classes/SecurityContextProviderInfo.cs
@@ -0,0 +1,28 @@
+﻿
+using YamlDotNet.Serialization;
+
+namespace Synapse.Core
+{
+    public class SecurityContextProviderInfo : ComponentInfoBase, ICloneable<SecurityContextProviderInfo>
+    {
+        public static readonly string DefaultType = "Synapse.Handlers.SecurityContext:Win32Impersonator";
+
+        //setting the Order causes StartInfo to be serialized after base class properties
+        [YamlMember( Order = 100 )]
+        new public SecurityContextProviderStartInfo StartInfo
+        {
+            get { return base.StartInfo as SecurityContextProviderStartInfo; }
+            set { base.StartInfo = value; }
+        }
+
+        public SecurityContextProviderInfo Clone(bool shallow = true)
+        {
+            return GetClone<SecurityContextProviderInfo>( shallow );
+        }
+
+        public static SecurityContextProviderInfo CreateSample()
+        {
+            return CreateSample<SecurityContextProviderInfo>( DefaultType );
+        }
+    }
+}
---
+++ Synapse.Core/Classes/SecurityContextProviderStartInfo.cs
@@ -0,0 +1,6 @@
+﻿namespace Synapse.Core
+{
+    public class SecurityContextProviderStartInfo : StartInfoBase
+    {
+    }
+}
